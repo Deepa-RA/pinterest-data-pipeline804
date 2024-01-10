d0b8b0a86713023f8887c22bcd2a542c20f1f59e
Deepa-RA <148036976+Deepa-RA@users.noreply.github.com>
2024-01-10 13:37:56 +0000

    update to user_posting_emultion

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -14,12 +14,7 @@ random.seed(100)
 class AWSDBConnector:
 
     def __init__(self):
-
-        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
-        self.USER = 'project_user'
-        self.PASSWORD = ':t%;yCY3Yjg'
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
+#credentials
         
     def create_db_connector(self):
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
