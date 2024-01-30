f4c65a0a41e0427c82a588356c3f121aa94549ca
Deepa-RA <148036976+Deepa-RA@users.noreply.github.com>
2024-01-30 12:55:16 +0000

    DAG file added

---
+++ Databricks_notebooks/128a59195de3_dag.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/ramaiya89@hotmail.com/Batch Processing - Databricks',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': '128a59195de3-deepa',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('128a59195de3_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 01, 30)
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+        
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
